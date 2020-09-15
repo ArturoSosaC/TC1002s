1ba6048a4f7a91b1ce892ac5508efbfb8ea42ac5
iArturoSosaC <iA01022687@itesm.mx>
2020-09-15 12:46:14 -0500

    Add vanilla_read()

--- io_utilities.py
+++ io_utilities.py
@@ -1,5 +1,12 @@
 # io_utilities.py
 
 def read_vanilla(filepath):
-    pass
-    return data
+
+    with open(filepath, 'r') as fp:
+        data = fp.read()
+        fp.close()
+
+dataLines = data.split('\n')
+dataFinal = [f.split(',') for f in dataLines]
+
+return dataFinal
